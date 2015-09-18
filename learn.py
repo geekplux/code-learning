a94e19c2c3f45bf4b55cfdc38638775d63fe6fc0
geekplux <geekplux@qq.com>
2015-09-18 17:26:41 +0800

    data type

--- learn.py
+++ learn.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python
 
+
+
+## print
+
 print('Hello World!')
 print('1 * 9 + 1 =', 1 * 9 + 1)
 
 print('please input something: ')
 _str = input()
 print('output', _str)
+
+print('I\'m learning Python.\n')
+
+## end print
+
+
+
+## data type
+
+_varied_type_data = (None, [True, False], {1, 2, 3}, {1: 'hello', 2: 'world'}, 88, 'ABC')
+print(type(_varied_type_data))
+
+for var in _varied_type_data:
+    print('{:<30} ==> {}'.format(str(var), type(var)))
+
+## end data_type
+
+
+
+
